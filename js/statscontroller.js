7cb06da97a911e8ebc115d9e773e72b7938bddc4
laisson.r.silveira <laisson.r.silveira@gmail.com>
2016-01-11 15:59:22 -0200

    Version 1.5 - Allow List of projects

--- js/statscontroller.js
+++ js/statscontroller.js
@@ -1,135 +1,155 @@
-function createSpinner() {
-    var opts = {
-        lines: 13, length: 20, width: 10, radius: 30, corners: 1, rotate: 0,
-        direction: 1, color: '#000', speed: 1, trail: 60, shadow: false, hwaccel: false,
-        className: 'spinner', zIndex: 2e9, top: '87%', left: '36%', scale: 0.2
-    };
-    new Spinner(opts).spin($('#spinner')[0]);
-}
-
-function setLabel() {
-    $('#version').text('v' + chrome.runtime.getManifest().version);
-    $('.name-project label').text(chrome.i18n.getMessage('txt_project_name_i18n'));
-    $('.name-project input').prop('placeholder', chrome.i18n.getMessage('txt_placeholder_project_name_i18n'));
-    $('.start-date label').text(chrome.i18n.getMessage('txt_start_date_i18n'));
-    $('.end-date label').text(chrome.i18n.getMessage('txt_end_date_i18n'));
-    $('#today').prop('label', chrome.i18n.getMessage('txt_today_i18n'));
-    $('#seven_days').prop('label', chrome.i18n.getMessage('txt_seven_days_i18n'));
-    $('#month').prop('label', chrome.i18n.getMessage('txt_month_i18n'));
-    $('#year').prop('label', chrome.i18n.getMessage('txt_year_i18n'));
-    $('#previous_month').prop('label', chrome.i18n.getMessage('txt_previous_month_i18n'));
-    $('.downloads').text(chrome.i18n.getMessage('txt_download_i18n'));
-    $('.operational_system').text(chrome.i18n.getMessage('txt_operational_system_i18n'));
-    $('.countries').text(chrome.i18n.getMessage('txt_countries_i18n'));
-    $('#top_country').text(chrome.i18n.getMessage('txt_top_country_i18n'));
-    $('#top_os').text(chrome.i18n.getMessage('txt_top_os_i18n'));
-    $('.percent').text(chrome.i18n.getMessage('txt_percent_download_i18n'));
-    $('#btn_find').text(chrome.i18n.getMessage('btn_find_stats_i18n'));
-    $('#btn_details').text(chrome.i18n.getMessage('btn_details_i18n'));
-    $('#btn_new_filter').text(chrome.i18n.getMessage('btn_new_filter_i18n'));
-    $('#btn_tab1').text(chrome.i18n.getMessage('btn_top_i18n'));
-    $('#btn_tab2').text(chrome.i18n.getMessage('btn_os_i18n'));
-    $('#btn_tab3').text(chrome.i18n.getMessage('btn_country_i18n'));
-    $('#btn_tab4').text(chrome.i18n.getMessage('btn_os_by_country_i18n'));
-}
-
-function setListDates() {
-    var today = new Date();
-    $('#today').val(convertDate(today));
-
-    var sevenDays = new Date();
-    sevenDays.setDate(today.getDate() - 7);
-    $('#seven_days').val(convertDate(sevenDays));
-
-    var month = new Date(today.getFullYear(), today.getMonth(), 1);
-    $('#month').val(convertDate(month));
-
-    var year = new Date(today.getFullYear(), 0, 1);
-    $('#year').val(convertDate(year));
-
-    var previousMonth = new Date();
-    previousMonth.setMonth(today.getMonth() - 1);
-    $('#previous_month').val(convertDate(previousMonth));
-}
-
-function convertDate(d) {
-    function normalize(s) { return (s < 10) ? '0' + s : s; }
-    return [d.getFullYear(), normalize(d.getMonth() + 1), normalize(d.getDate())].join('-');
-}
-
-function loadChart(stats) {
-    var dateChart = ['x'], downloadsChart = [chrome.i18n.getMessage('txt_legend_chart_i18n')];
-
-    for (var i = 0; i < stats.downloads.length; i++) {
-        var dateLegend = new Date(stats.downloads[i][0]);
-        dateChart.push(convertDate(dateLegend));
-        downloadsChart.push(stats.downloads[i][1]);
-    }
-
-    var chartDownloads = c3.generate({
-        bindto: '#chartDownloads',
-        data: {
-            x: 'x',
-            columns: [
-                dateChart,
-                downloadsChart
-            ]
-        },
-        axis: {
-            x: {
-                type: 'timeseries',
-                tick: {
-                    format: '%Y-%m-%d'
-                }
-            },
-            y: {
-                tick: {
-                    format: d3.format('d')
-                }
-            }
-        }
-    });
-
-    var chartTopOS = c3.generate({
-        bindto: '#chartTopOS',
-        size: {
-            width: 180,
-            height: 180
-        },
-        data: {
-            columns: stats.oses,
-            type : 'pie'
-        },
-        legend: {
-            hide: true
-        }
-    });
-
-    var chartTopCountry = c3.generate({
-        bindto: '#chartTopCountry',
-        size: {
-            width: 180,
-            height: 180
-        },
-        data: {
-            columns: stats.countries,
-            type : 'pie'
-        },
-        legend: {
-            hide: true
-        }
-    });
-}
-
-function dateDiffInDays(a, b) {
-    var utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
-    var utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
-
-    return Math.floor((utc2 - utc1) / ( 1000 * 60 * 60 * 24));
-}
-
 var myapp = angular.module('SFStatsApp', []);
 myapp.controller('StatsController', function ($scope, $http, $filter) {
+
+    function initLocalStorage() {
+        var savedProject = localStorage.getItem("nameProject");
+        if (savedProject == null) {
+            return;
+        } else {
+            var projects = new Array(savedProject);
+            localStorage.setItem("projects", JSON.stringify(projects));
+            $scope.projects = projects;
+        }
+
+        localStorage.removeItem("nameProject");
+        localStorage.removeItem("name_project");
+
+        var savedProjects = localStorage.getItem("projects");
+        if (savedProjects == null) {
+            var projects = new Array();
+            localStorage.setItem("projects", JSON.stringify(projects));
+        }
+    }
+
+    function createSpinner() {
+        var opts = {
+            lines: 13, length: 20, width: 10, radius: 30, corners: 1, rotate: 0,
+            direction: 1, color: '#000', speed: 1, trail: 60, shadow: false, hwaccel: false,
+            className: 'spinner', zIndex: 2e9, top: '87%', left: '36%', scale: 0.2
+        };
+        new Spinner(opts).spin($('#spinner')[0]);
+    }
+
+    function setLabel() {
+        $('#version').text('v' + chrome.runtime.getManifest().version);
+        $('.name-project label').text(chrome.i18n.getMessage('txt_project_name_i18n'));
+        $('.name-project input').prop('placeholder', chrome.i18n.getMessage('txt_placeholder_project_name_i18n'));
+        $('.start-date label').text(chrome.i18n.getMessage('txt_start_date_i18n'));
+        $('.end-date label').text(chrome.i18n.getMessage('txt_end_date_i18n'));
+        $('#today').prop('label', chrome.i18n.getMessage('txt_today_i18n'));
+        $('#seven_days').prop('label', chrome.i18n.getMessage('txt_seven_days_i18n'));
+        $('#month').prop('label', chrome.i18n.getMessage('txt_month_i18n'));
+        $('#year').prop('label', chrome.i18n.getMessage('txt_year_i18n'));
+        $('#previous_month').prop('label', chrome.i18n.getMessage('txt_previous_month_i18n'));
+        $('.downloads').text(chrome.i18n.getMessage('txt_download_i18n'));
+        $('.operational_system').text(chrome.i18n.getMessage('txt_operational_system_i18n'));
+        $('.countries').text(chrome.i18n.getMessage('txt_countries_i18n'));
+        $('#top_country').text(chrome.i18n.getMessage('txt_top_country_i18n'));
+        $('#top_os').text(chrome.i18n.getMessage('txt_top_os_i18n'));
+        $('.percent').text(chrome.i18n.getMessage('txt_percent_download_i18n'));
+        $('#btn_find').text(chrome.i18n.getMessage('btn_find_stats_i18n'));
+        $('#btn_details').text(chrome.i18n.getMessage('btn_details_i18n'));
+        $('#btn_new_filter').text(chrome.i18n.getMessage('btn_new_filter_i18n'));
+        $('#btn_tab1').text(chrome.i18n.getMessage('btn_top_i18n'));
+        $('#btn_tab2').text(chrome.i18n.getMessage('btn_os_i18n'));
+        $('#btn_tab3').text(chrome.i18n.getMessage('btn_country_i18n'));
+        $('#btn_tab4').text(chrome.i18n.getMessage('btn_os_by_country_i18n'));
+    }
+
+    function setListDates() {
+        var today = new Date();
+        $('#today').val(convertDate(today));
+
+        var sevenDays = new Date();
+        sevenDays.setDate(today.getDate() - 7);
+        $('#seven_days').val(convertDate(sevenDays));
+
+        var month = new Date(today.getFullYear(), today.getMonth(), 1);
+        $('#month').val(convertDate(month));
+
+        var year = new Date(today.getFullYear(), 0, 1);
+        $('#year').val(convertDate(year));
+
+        var previousMonth = new Date();
+        previousMonth.setMonth(today.getMonth() - 1);
+        $('#previous_month').val(convertDate(previousMonth));
+    }
+
+    function convertDate(d) {
+        function normalize(s) { return (s < 10) ? '0' + s : s; }
+        return [d.getFullYear(), normalize(d.getMonth() + 1), normalize(d.getDate())].join('-');
+    }
+
+    function loadChart(stats) {
+        var dateChart = ['x'], downloadsChart = [chrome.i18n.getMessage('txt_legend_chart_i18n')];
+
+        for (var i = 0; i < stats.downloads.length; i++) {
+            var dateLegend = new Date(stats.downloads[i][0]);
+            dateChart.push(convertDate(dateLegend));
+            downloadsChart.push(stats.downloads[i][1]);
+        }
+
+        var chartDownloads = c3.generate({
+            bindto: '#chartDownloads',
+            data: {
+                x: 'x',
+                columns: [
+                    dateChart,
+                    downloadsChart
+                ]
+            },
+            axis: {
+                x: {
+                    type: 'timeseries',
+                    tick: {
+                        format: '%Y-%m-%d'
+                    }
+                },
+                y: {
+                    tick: {
+                        format: d3.format('d')
+                    }
+                }
+            }
+        });
+
+        var chartTopOS = c3.generate({
+            bindto: '#chartTopOS',
+            size: {
+                width: 180,
+                height: 180
+            },
+            data: {
+                columns: stats.oses,
+                type : 'pie'
+            },
+            legend: {
+                hide: true
+            }
+        });
+
+        var chartTopCountry = c3.generate({
+            bindto: '#chartTopCountry',
+            size: {
+                width: 180,
+                height: 180
+            },
+            data: {
+                columns: stats.countries,
+                type : 'pie'
+            },
+            legend: {
+                hide: true
+            }
+        });
+    }
+
+    function dateDiffInDays(a, b) {
+        var utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
+        var utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
+
+        return Math.floor((utc2 - utc1) / ( 1000 * 60 * 60 * 24));
+    }
 
     function setDefaultValues() {
         $scope.stats = null;
@@ -156,7 +176,11 @@ myapp.controller('StatsController', function ($scope, $http, $filter) {
             $scope.containsInfo = true;
             $scope.showChartDownloads = true;
             $scope.showForm = false;
-            localStorage["nameProject"] = $scope.filter.nameProject;
+
+            var projectsLS = JSON.parse(localStorage.getItem("projects"));
+            projectsLS.push($scope.filter.nameProject);
+            localStorage.setItem("projects", JSON.stringify(projectsLS));
+            $scope.projects = projectsLS;
         }).error(function () {
             $scope.showSpinner = false;
             setDefaultValues();
@@ -167,11 +191,13 @@ myapp.controller('StatsController', function ($scope, $http, $filter) {
     }
 
     $scope.filter = {
-        nameProject: localStorage["nameProject"],
+        nameProject: '',
         startDate: new Date(),
         endDate: new Date()
     };
 
+    $scope.projects = JSON.parse(localStorage.getItem("projects"));
+    
     $scope.validateForm = function () {
 
         $scope.error = null;
@@ -218,6 +244,7 @@ myapp.controller('StatsController', function ($scope, $http, $filter) {
         }
     };
 
+    initLocalStorage();
     setLabel();
     setListDates();
     setDefaultValues();
